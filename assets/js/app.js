ac8219245d52c78e3b8f3a071b7e80e7d68cb97b
evaleries <me@didaarda.co.uk>
2019-11-19 22:53:53 +0700

    Add Lazy Load
    [+] Improve web speed
    [+] Link login href

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  let active = false;
+
+  const lazyLoad = function() {
+    if (active === false) {
+      active = true;
+
+      setTimeout(function() {
+        lazyImages.forEach(function(lazyImage) {
+          if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.srcset = lazyImage.dataset.srcset;
+            lazyImage.classList.remove("lazy");
+
+            lazyImages = lazyImages.filter(function(image) {
+              return image !== lazyImage;
+            });
+
+            if (lazyImages.length === 0) {
+              $(document).off('scroll');
+              $(window).off('resize');
+            }
+          }
+        });
+
+        active = false;
+      }, 200);
+    }
+  };
+
+  $(document).on('scroll', lazyLoad)
+  $(window).on('resize', lazyLoad)
+});
